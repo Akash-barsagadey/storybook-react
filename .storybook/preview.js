22b916dbb1b638c5b0448560fc1759776223f4b3
Akash Barsagadey <akash_udaram.barsagadey@syngenta.com>
2020-06-30 18:13:46 +0530

    added live edit addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
 import "../src/index.css";
 import { addParameters } from "@storybook/react";
+import { setAddon } from "@storybook/react";
+import LiveEdit, { setOptions } from "storybook-addon-react-live-edit";
+
+setOptions({ presets: ["react"] });
+
+setOptions({ theme: "darcula", presets: ["react"] });
+
+setAddon(LiveEdit);
+
 addParameters({
   backgrounds: [
     { name: "twitter", value: "#00aced", default: true },
--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -2,14 +2,25 @@ import React from "react";
 import { action } from "@storybook/addon-actions";
 import { Button } from "@storybook/react/demo";
 
+import {
+  withLiveEditScope,
+  withLiveEdit,
+} from "storybook-addon-react-live-edit";
+
 export default {
   title: "Button",
   component: Button,
+  decorators: [
+    (story) => withLiveEditScope({ React, Button })(story, { React, Button }),
+  ],
 };
 
 export const Text = () => (
   <Button onClick={action("clicked")}>Hello Button</Button>
 );
+
+export const LiveEditComponent = (story) =>
+  withLiveEdit(`return <Button >Hello Button</Button>`)(story);
 
 export const Emoji = () => (
   <Button onClick={action("clicked")}>
